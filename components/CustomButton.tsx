da75479b56be2a894d11a10679c01eee1ee373fa
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-13 23:39:35 +0600

    first commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+const CustomButton = () => {
+  return (
+    <button disabled={false} type={'button'}>CustomButton</button>
+  )
+}
+
+export default CustomButton

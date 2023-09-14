fd15b970146a370f0fdf1f31a528ec86e31c5048
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-14 23:46:04 +0600

    add apis

---
+++ utils/index.ts
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+  params: {model: 'corolla'},
+  headers: {
+    'X-RapidAPI-Key': '8ff8c2b6e6mshd65b7ac41af864bp1ac3d3jsna5f0535361c6',
+    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  }
+};
+
+try {
+	const response = await axios.request(options);
+	console.log(response.data);
+} catch (error) {
+	console.error(error);
+}

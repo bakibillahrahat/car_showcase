68c4a4075d4de4adc8320733b5112f028da47d52
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-14 23:57:06 +0600

    update api integration

--- utils/index.ts
+++ utils/index.ts
@@ -1,27 +1,15 @@
-const axios = require("axios");
-
-const options = {
-  method: "GET",
-  url: "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
-  params: { model: "corolla" },
-  headers: {
+export async function fetchCars() {
+  const headers = {
     "X-RapidAPI-Key": "8ff8c2b6e6mshd65b7ac41af864bp1ac3d3jsna5f0535361c6",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  },
-};
-
-try {
-  const response = await axios.request(options);
-  console.log(response.data);
-} catch (error) {
-  console.error(error);
-}
-
-
-export async function fetchCars() {
-    const headers = {
-        "X-RapidAPI-Key": "8ff8c2b6e6mshd65b7ac41af864bp1ac3d3jsna5f0535361c6",
-        "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+    {
+      headers: headers,
     }
-    const response = await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?");
+  );
+
+  const result = await response.json();
+  return result;
 }

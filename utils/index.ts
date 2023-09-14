822f0212bd97a9c226d4812b3359feffc7b89342
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-14 23:54:00 +0600

    api intigration

--- utils/index.ts
+++ utils/index.ts
@@ -1,18 +1,27 @@
-const axios = require('axios');
+const axios = require("axios");
 
 const options = {
-  method: 'GET',
-  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-  params: {model: 'corolla'},
+  method: "GET",
+  url: "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+  params: { model: "corolla" },
   headers: {
-    'X-RapidAPI-Key': '8ff8c2b6e6mshd65b7ac41af864bp1ac3d3jsna5f0535361c6',
-    'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-  }
+    "X-RapidAPI-Key": "8ff8c2b6e6mshd65b7ac41af864bp1ac3d3jsna5f0535361c6",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  },
 };
 
 try {
-	const response = await axios.request(options);
-	console.log(response.data);
+  const response = await axios.request(options);
+  console.log(response.data);
 } catch (error) {
-	console.error(error);
+  console.error(error);
+}
+
+
+export async function fetchCars() {
+    const headers = {
+        "X-RapidAPI-Key": "8ff8c2b6e6mshd65b7ac41af864bp1ac3d3jsna5f0535361c6",
+        "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    }
+    const response = await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?");
 }

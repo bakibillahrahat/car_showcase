0cacbaab31b37f237c68a7eb5c0fd0ff7501357e
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-14 23:43:24 +0600

    update

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,12 +5,12 @@ import { useState } from "react";
 import { SearchMenufacturer } from "./";
 
 const SearchBar = () => {
-    const [manuFacturer, setManuFacturer] = useState("");
+    const [manufacturer, setManuFacturer] = useState("");
     const handleSearch = () => {}
   return (
     <form className='searchbar' onSubmit={handleSearch}>
         <div className="searchbar__item">
-            <SearchMenufacturer manufacturer={manuFacturer} setManuFacturer={setManuFacturer}/>
+            <SearchMenufacturer manufacturer={manufacturer} setManuFacturer={setManuFacturer}/>
         </div>
     </form>
   )

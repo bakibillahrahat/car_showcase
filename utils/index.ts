c027941d3d19af3a522541a9628b6bbdc1d5c0ac
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-16 15:54:40 +0600

    integrate image api

--- utils/index.ts
+++ utils/index.ts
@@ -6,7 +6,7 @@ export async function fetchCars() {
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
   const response = await fetch(
-    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3",
     {
       headers: headers,
     }
@@ -17,16 +17,25 @@ export async function fetchCars() {
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-    const basePricePerDay = 50; // Base rental price per day
-    const mileageFactor = 0.1;
-    const ageFactor = 0.05;
-    const mileageRate = city_mpg * mileageFactor;
-    const ageRate = (new Date().getFullYear() - year) * ageFactor;
-    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  const basePricePerDay = 50; // Base rental price per day
+  const mileageFactor = 0.1;
+  const ageFactor = 0.05;
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
-    return rentalRatePerDay.toFixed(0);
-}
+  return rentalRatePerDay.toFixed(0);
+};
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-  
-}
+  const url = new URL("https://cdn.imagin.studio/get-image");
+  const { make, year, model } = car;
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+};
